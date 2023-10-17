c9e85732b3656803c45d0e07ad1a81a5a939f8f3
Haakon Meland Eriksen <haakon.eriksen@far.no>
2023-10-17 22:07:18 +0200

    Changed pull_model.clj to MODEL_REPO and model_repo rather than ollama to allow setting either Ollama model repo or Hugging Face model repo. NOTE: Needs to fix feedback when running docker compose or docker-compose as it takes a long time to fetch large language models. Needs to fix image from ollama to something neutral like model.

--- pull_model.clj
+++ pull_model.clj
@@ -4,12 +4,13 @@
 
 (try
   (let [llm (get (System/getenv) "LLM")
-        url (get (System/getenv) "OLLAMA_BASE_URL")]
-    (println (format "pulling ollama model %s using %s" llm url))
+        url (get (System/getenv) "MODEL_REPO_BASE_URL")]
+    (println (format "pulling model %s using %s" llm url))
     (if (and llm url (not (#{"gpt-4" "gpt-3.5"} llm)))
 
       ;; ----------------------------------------------------------------------
-      ;; just call `ollama pull` here - create OLLAMA_HOST from OLLAMA_BASE_URL
+
+      ;; just call `model pull` here - create MODEL_HOST from MODEL_REPO_BASE_URL
       ;; ----------------------------------------------------------------------
       ;; TODO - this still doesn't show progress properly when run from docker compose
 
@@ -17,8 +18,8 @@
         (async/go-loop [n 0]
           (let [[v _] (async/alts! [done (async/timeout 5000)])]
             (if (= :stop v) :stopped (do (println (format "... pulling model (%ss) - will take several minutes" (* n 10))) (recur (inc n))))))
-        (process/shell {:env {"OLLAMA_HOST" url} :out :inherit :err :inherit} (format "./bin/ollama pull %s" llm))
+        (process/shell {:env {"MODEL_HOST" url} :out :inherit :err :inherit} (format "./bin/ollama pull %s" llm))
         (async/>!! done :stop))
 
-      (println "OLLAMA model only pulled if both LLM and OLLAMA_BASE_URL are set and the LLM model is not gpt")))
+      (println "MODEL_REPO model only pulled if both LLM and MODEL_REPO_BASE_URL are set and the LLM model is not gpt")))
   (catch Throwable _ (System/exit 1)))
